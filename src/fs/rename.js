7911a9bdc98f805603603989316d9aa48e5db53a
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-05 14:03:49 +0300

    feat: implement rename.js function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const oldFilePath = path.join(process.cwd(), 'src', 'fs', 'files', 'wrongFilename.txt');
+  const newFilePath = path.join(process.cwd(), 'src', 'fs', 'files', 'properFilename.md');
+
+  try {
+    const oldFileExists = await fs.stat(oldFilePath).catch(() => false);
+    if (!oldFileExists) {
+      throw new Error('FS operation failed');
+    }
+
+    const newFileExists = await fs.stat(newFilePath).catch(() => false);
+    if (newFileExists) {
+      throw new Error('FS operation failed');
+    }
+
+    await fs.rename(oldFilePath, newFilePath);
+    console.log('File renamed successfully');
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await rename();
