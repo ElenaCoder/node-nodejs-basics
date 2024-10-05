68544b62954731255c2edcf2c2188da248edb2c5
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-05 13:46:19 +0300

    feat: implement copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const sourceFolderPath = path.join(process.cwd(), 'src', 'fs', 'files');
+  const destinationFolderPath = path.join(process.cwd(), 'src', 'fs', 'files_copy');
+
+  try {
+    const sourceFolderExists = await fs.stat(sourceFolderPath).catch(() => false);
+    if (!sourceFolderExists) {
+      throw new Error('FS operation failed');
+    }
+
+    const destinationFolderExists = await fs.stat(destinationFolderPath).catch(() => false);
+    if (destinationFolderExists) {
+      throw new Error('FS operation failed');
+    }
+
+    await fs.cp(sourceFolderPath, destinationFolderPath, { recursive: true });
+    console.log('Folder files_copy copied successfully');
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
+// Execute the copy function
 await copy();
