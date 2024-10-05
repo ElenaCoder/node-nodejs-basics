ed52129196c21b1743e1c9c40670ce4bc170a746
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-05 18:52:10 +0300

    feat: implement args.js function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    const args = process.argv.slice(2);
 
-parseArgs();
+    let output = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+      const key = args[i].replace('--', '');
+      const value = args[i + 1];
+      output.push(`${key} is ${value}`);
+    }
+
+    console.log(output.join(', '));
+  };
+
+  parseArgs();
