f7a678daad256a8361b01dec46d33d9bc07fb0f3
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-05 21:16:03 +0300

    feat: implement write.js function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(process.cwd(), 'src', 'streams', 'files', 'fileToWrite.txt');
+
+  const writeStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log('Data written successfully');
+  });
+
+  writeStream.on('error', (err) => {
+    console.error('Error writing to file:', err.message);
+  });
 };
 
 await write();
